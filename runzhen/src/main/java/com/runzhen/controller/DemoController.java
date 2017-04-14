da346348cd7bf9675d22f49db1e7c1379f583265
TongJian <hubeitongjian@163.com>
2017-04-14 23:19:38 +0800

    2017-4-14 阿里云 --童键

--- runzhen/src/main/java/com/runzhen/controller/DemoController.java
+++ runzhen/src/main/java/com/runzhen/controller/DemoController.java
@@ -1,13 +1,19 @@
 package com.runzhen.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DemoController {
 
-	@RequestMapping("/index")
+	private static final Log logger = LogFactory.getLog(DemoController.class);
+	
+	@RequestMapping("/hello")
 	public String index(){
+		logger.info("DemoController called");
 		return "demo";
 	}
+
 }
